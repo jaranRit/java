b9031a5e97a75a7ac6df9c3ed878d2f1825ec0d2
jaranRit <jarannrit10@gmail.com>
2018-04-30 23:36:48 +0700

    java

---
+++ java/src/java/jfreme.java
@@ -0,0 +1,46 @@
+package java;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class jfreme extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					jfreme frame = new jfreme();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public jfreme() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblHelloWorld = new JLabel("Hello World");
+		lblHelloWorld.setBounds(206, 78, 77, 28);
+		contentPane.add(lblHelloWorld);
+	}
+}
